7f64ee7081357a121c9cdc593327218627876f74
hdi10 <s0551006@htw-berlin.de>
2022-12-13 04:40:09 +0100

    Worker Queues --> NewTask.class and Worker.class

---
+++ src/test/java/Worker.java
@@ -0,0 +1,59 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Dastekin created on 13.12.2022 the Worker-Class inside the package - PACKAGE_NAME
+ */
+public class Worker {
+
+    private final static String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        String hier = "Worker";
+        System.out.println(" [*] Waiting for messages in " + hier + ". To exit press CTRL+C");
+
+        channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+
+            System.out.println(" [x] Received '" + message + "'");
+
+            try {
+                doWork(message);
+            } finally {
+                System.out.println(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {
+        });
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
