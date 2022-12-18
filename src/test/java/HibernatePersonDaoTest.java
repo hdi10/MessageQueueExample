01e72a73e9ce867d63d681e69d55ecd5740c59ba
hdi10 <s0551006@htw-berlin.de>
2022-12-18 02:58:56 +0100

    continue working HibernatePersonDaoTest.java

--- src/test/java/HibernatePersonDaoTest.java
+++ src/test/java/HibernatePersonDaoTest.java
@@ -1,8 +1,14 @@
+import core.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import javax.management.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.internal.util.MockUtil.createMock;
 
 /**
  * Dastekin created on 18.12.2022 the HibernatePersonDaoTest-Class inside the package - PACKAGE_NAME
@@ -21,4 +27,19 @@ private Query query;
     query = createMock(Query.class);
 
 }
+
+@Test
+    public  void testFindLastname() throws Exception {
+    String hql = "from Person p where p.lastname = :lastname";
+    String lastname = "Smith";
+
+    List<Person> theSmiths = new ArrayList<Person>();
+    theSmiths.add(new Person("Alice",lastname);
+    theSmiths.add(new Person("Billy",lastname);
+    theSmiths.add(new Person("Clark",lastname);
+
 }
+
+
+}
+
